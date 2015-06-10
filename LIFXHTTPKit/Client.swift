65f4a683d4dad90dcf611635ecce6f5cf21852fd
Tate Johnson <tate@tatey.com>
2015-06-11 05:26:54 +1200

    Build out HTTPSession for talking to the HTTP API.

--- LIFXHTTPKit/Client.swift
+++ LIFXHTTPKit/Client.swift
@@ -9,12 +9,12 @@
 import Foundation
 
 public class Client {
-	private let accessToken: String
+	private let session: HTTPSession
 	private var lights: [Light]
 	private var observers: [LightTarget]
 
 	public init(accessToken: String) {
-		self.accessToken = accessToken
+		session = HTTPSession(accessToken: accessToken)
 		lights = []
 		observers = []
 	}
@@ -38,7 +38,7 @@ public class Client {
 	}
 
 	public func allLights() -> LightTarget {
-		return LightTarget(client: self, filter: { (light: Light) -> Bool in return true })
+		return LightTarget(client: self, filter: { (light) in return true })
 	}
 
 	public func allGroups() -> [LightTarget] {
@@ -50,24 +50,13 @@ public class Client {
 	}
 }
 
-public struct Light: Equatable {
-	public let id: String
-	public let label: String
-	public let power: Bool
-	public let brightness: Float
-}
-
-public func ==(lhs: Light, rhs: Light) -> Bool {
-	return lhs.id == rhs.id
-}
-
 public class LightTarget {
 	public typealias Filter = (light: Light) -> Bool
 
 	public var power: Bool
 	public var brightness: Float
 
-	private weak var client: Client?
+	private unowned var client: Client
 	private let filter: Filter
 	private var lights: [Light]
 
@@ -82,7 +71,7 @@ public class LightTarget {
 	}
 
 	deinit {
-		client?.removeObserver(self)
+		client.removeObserver(self)
 	}
 
 	internal func setLights(lights: [Light]) {
@@ -92,4 +81,68 @@ public class LightTarget {
 	public func toLights() -> [Light] {
 		return lights
 	}
+}
+
+public class HTTPSession {
+	private let accessToken: String
+	private let baseURL: NSURL
+	private let userAgent: String
+	private let session: NSURLSession
+
+	public init(accessToken: String, baseURL: NSURL = NSURL(string: "https://api.lifx.com/v1beta1/")!, userAgent: String = "LIFXHTTPKit/0.0.1") {
+		self.accessToken = accessToken
+		self.baseURL = baseURL
+		self.userAgent = userAgent
+		session = NSURLSession.sharedSession()
+	}
+
+	public func lights(selector: String = "all", completionHander: ((request: NSURLRequest, response: NSURLResponse, lights: [Light], error: NSError?) -> Void)) {
+		let url = baseURL.URLByAppendingPathComponent("/lights/\(selector)")
+		let request = NSMutableURLRequest(URL: url)
+		request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+		request.setValue("application/json", forHTTPHeaderField: "Accept")
+		request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
+		session.dataTaskWithRequest(request) { (data, response, error) in
+			if error != nil {
+				completionHander(request: request, response: response, lights: [], error: error)
+			} else {
+				let deserialized = self.dataToLights(data)
+				completionHander(request: request, response: response, lights: deserialized.lights, error: deserialized.error)
+			}
+		}
+	}
+
+	private func dataToLights(data: NSData) -> (lights: [Light], error: NSError?) {
+		return ([], nil)
+	}
+
+	private func dataToResults(data: NSData) -> (results: [Result], error: NSError?) {
+		return ([], nil)
+	}
+}
+
+public struct Light: Equatable {
+	public let id: String
+	public let label: String
+	public let power: Bool
+	public let brightness: Float
+}
+
+public func ==(lhs: Light, rhs: Light) -> Bool {
+	return lhs.id == rhs.id
+}
+
+public struct Result: Equatable {
+	public enum Status {
+		case OK
+		case TimedOut
+		case Offline
+	}
+
+	public let id: String
+	public let status: Status
+}
+
+public func ==(lhs: Result, rhs: Result) -> Bool {
+	return lhs.id == rhs.id && lhs.status == rhs.status
 }
