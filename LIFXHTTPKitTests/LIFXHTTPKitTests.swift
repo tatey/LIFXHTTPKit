b79525eb6d5ad00e1656acbe492618f60262d425
Tate Johnson <tate@tatey.com>
2015-06-13 15:10:23 +1200

    Remove placeholder tests.

--- LIFXHTTPKitTests/LIFXHTTPKitTests.swift
+++ LIFXHTTPKitTests/LIFXHTTPKitTests.swift
@@ -1,38 +1,12 @@
 //
-//  LIFXHTTPKitTests.swift
-//  LIFXHTTPKitTests
-//
 //  Created by Tate Johnson on 29/05/2015.
 //  Copyright (c) 2015 Tate Johnson. All rights reserved.
 //
 
-import Cocoa
 import XCTest
 import LIFXHTTPKit
 
 class LIFXHTTPKitTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 	func testClient() {
 		let expectation = expectationWithDescription("")
 		let client = Client(accessToken: "")
